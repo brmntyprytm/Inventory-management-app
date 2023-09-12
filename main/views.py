c7612e11655db19ad4360b10f59d9657c25afae5
brmntyprytm <bramantyopriyou@gmail.com>
2023-09-12 22:12:05 +0700

    Updated MVT

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 def show_main(request):
-    context = {"name": "Bramantyo", "class": "PBP International"}
+    context = {
+        "name": "Bramantyo",
+        "class": "PBP International",
+    }
 
     return render(request, "main.html", context)
